41b0f84ce2d30529fb9c0f7afcf5eb5c2f2d8d8b
GARAKOTI <rahulsinghgarakoti730@gmail.com>
2024-03-10 00:53:29 +0530

    "Added the product component and its styles "

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ProductCart from '../components/product-cart'
 
 const Home = () => {
+
+  const addToCartHandler =()=>{};
   return (
     <div className='home'>
       <section className='main'></section>
@@ -11,6 +14,9 @@ const Home = () => {
           MORE
         </Link>
         </h1>
+        <main>
+          <ProductCart productId='adaaaddd' name='Mackbook' price={122333} stock={2300} handler={addToCartHandler} photo='https://m.media-amazon.com/images/I/71vFKBpKakL._AC_UY218_.jpg'/>
+        </main>
  
     </div>                                                                                    
   )
